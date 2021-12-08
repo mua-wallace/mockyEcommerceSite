2d1aaa9268ec9c7599e160fae55ad84bbecd3430
mua-wallace <muawallizewi@gmail.com>
2021-12-08 17:58:51 +0100

    created mocky-dashboard

---
+++ mocky-dashboard/src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import {Button} from 'react-bootstrap';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to the mocky-Ecommerce site</h1>
+      <button>Simple button</button>
+      <Button>Bootstrap button</Button>
+    </div>
+  );
+}
+
+export default App;
