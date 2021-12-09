1edcff894c05e72d71674a5d99e9cbeea63500ee
mua-wallace <muawallizewi@gmail.com>
2021-12-09 15:50:59 +0100

    integration of  form with backen

--- mocky-dashboard/src/Register.js
+++ mocky-dashboard/src/Register.js
@@ -1,7 +1,40 @@
+import React, {useState} from 'react';
+import {useHistory} from 'react-router-dom';
 function Register() {
+
+    const [name,setName] = useState("");
+    const [email,setEmail] = useState("");
+    const [password,setPassword] = useState("");
+    const history = useHistory();
+
+   async function signup(){
+        let item = {name, email, password};
+
+      let result = await fetch("http://localhost:8000/api/register", {
+            method:'POST',
+            body:JSON.stringify(item),
+            headers:{
+                "Content-Type":'application/json',
+                "Accept": 'application/json'
+            }
+        })
+        result = await result.json();
+        // console.warn("reult", result);
+        localStorage.setItem('user-info', JSON.stringify(result));
+        history.push("/add");
+
+    }
+
     return (
-        <div>
+        <div className="col-sm-6 offset-sm-3">
             <h1>Register Page</h1>
+            <input type="text" value={name} onChange={(e) => setName(e.target.value)} className="form-control" placeholder="name"></input>
+             <br />
+             <input type="text" value={email} onChange={(e)=>setEmail(e.target.value)} className="form-control" placeholder="email"></input>
+             <br />
+             <input type="password" value={password} onChange={(e)=>setPassword(e.target.value)} className="form-control" placeholder="password"></input>
+             <br />
+            <button onClick={signup} className="btn btn-primary">Sign Up</button>
         </div>
     )
 }
