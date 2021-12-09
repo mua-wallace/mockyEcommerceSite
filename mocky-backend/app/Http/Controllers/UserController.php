be9f06739501625cb3832e04c343b5cc87992351
mua-wallace <muawallizewi@gmail.com>
2021-12-09 15:52:49 +0100

    created the register function

--- mocky-backend/app/Http/Controllers/UserController.php
+++ mocky-backend/app/Http/Controllers/UserController.php
@@ -3,10 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
-    function register() {
-        return "Hello from register";
+    function register(Request $req) {
+        $user = new User;
+        $user->name = $req->input('name');
+        $user->email = $req->input('email');
+        $user->password = Hash::make($req->input('password'));
+        $user->save();
+        return $user;
     }
 }
