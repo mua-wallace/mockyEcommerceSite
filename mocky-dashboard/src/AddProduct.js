86097bb0fd7d22a585be54dc8ee982c4c2a3e115
mua-wallace <muawallizewi@gmail.com>
2021-12-13 14:50:29 +0100

    moddified addProduct

--- mocky-dashboard/src/AddProduct.js
+++ mocky-dashboard/src/AddProduct.js
@@ -1,10 +1,47 @@
 import Header from "./Header";
-
+import {useState} from 'react';
 function AddProduct() {
+    const [name, setName] = useState("");
+    const [category, setCategory] = useState("");
+    const [image, setImage] = useState("");
+    const [description, setDescription] = useState("");
+    const [price, setPrice] = useState("");
+    const [quantity, setQuatity] = useState("");
+    const [delivery_date, setDelivery_date] = useState("");
+    async function addProduct() {
+        console.warn(name, category,image,description,price, quantity, delivery_date);
+        const FormData  = new FormData();
+     FormData.append('image', image);
+     FormData.append('name', name);
+     FormData.append('category', category);
+     FormData.append('description', description);
+     FormData.append('price', price);
+     FormData.append('quantity', quantity);
+     FormData.append('delivery_date', delivery_date);
+        let result = await fetch("http://localhost:8000/api/addProduct", {
+            method: "POST", 
+            body FormData;
+            headers:{
+                
+            }
+        });
+        console.warn(result);
+        alert('data has been saved');
+    }
     return (
         <div>
-        <Header />
-        <input type="text" className="form-control"/>
+            <Header />
+            <div className="col-sm-6 offset-sm-3">
+            <br />
+            <input type="text" onChange={(e) =>setName(e.target.value)} className="form-control" placeholder="name"/> <br />
+            <input type="text" onChange={(e)=>setCategory(e.target.value)} className="form-control" placeholder="category"/> <br />
+            <input type="file" onChange={(e)=>setImage(e.target.files[0])} className="form-control" placeholder="image"/> <br />
+            <input type="text" onChange={(e)=>setDescription(e.target.value)} className="form-control" placeholder="description"/> <br />
+            <input type="text" onChange={(e)=>setPrice(e.target.value)} className="form-control" placeholder="price"/> <br />
+            <input type="text" onChange={(e)=>setQuatity(e.target.value)} className="form-control" placeholder="quantity"/> <br />
+            <input type="text" onChange={(e)=>setDelivery_date(e.target.value)} className="form-control" placeholder="delivery date"/> <br />
+            <button onClick={addProduct} className="btn btn-primary" >Add Product</button>
+            </div>
         </div>
     )
 }
