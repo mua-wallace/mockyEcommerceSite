c5bc0127eaf475d0e3feb0fe00d550f3bc4b1a77
mua-wallace <muawallizewi@gmail.com>
2021-12-15 00:55:29 +0100

    modified addProduct

--- mocky-dashboard/src/AddProduct.js
+++ mocky-dashboard/src/AddProduct.js
@@ -1,5 +1,8 @@
 import Header from "./Header";
-import {useState} from 'react';
+import { useState } from 'react';
+import { API_URL } from "./config/api";
+
+
 function AddProduct() {
     const [name, setName] = useState("");
     const [category, setCategory] = useState("");
@@ -9,38 +12,38 @@ function AddProduct() {
     const [quantity, setQuatity] = useState("");
     const [delivery_date, setDelivery_date] = useState("");
     async function addProduct() {
-        console.warn(name, category,image,description,price, quantity, delivery_date);
-        const bodyFormData  = new FormData();
-     bodyFormData.append('image', image);
-     bodyFormData.append('name', name);
-     bodyFormData.append('category', category);
-     bodyFormData.append('description', description);
-     bodyFormData.append('price', price);
-     bodyFormData.append('quantity', quantity);
-     bodyFormData.append('delivery_date', delivery_date);
-        let result = await fetch("http://localhost:8000/api/addProduct", {
-            method: "POST", 
-            body: bodyFormData,
-            headers: {
-                "Content-Type":'multipart/form-data'
-            }
+
+
+        // console.warn(name, category,image,description, price,quantity, delivery_date);
+        const formData = new FormData();
+        formData.append('image', image);
+        formData.append('name', name);
+        formData.append('category', category);
+        formData.append('description', description);
+        formData.append('price', price);
+        formData.append('quantity', quantity);
+        formData.append('delivery_date', delivery_date);
+        let result = await fetch(API_URL + "addproduct", {
+            method: "POST",
+            body: formData,
         });
         console.warn(result);
+
         alert('data has been saved');
     }
     return (
         <div>
             <Header />
             <div className="col-sm-6 offset-sm-3">
-            <br />
-            <input type="text" onChange={(e) =>setName(e.target.value)} className="form-control" placeholder="name"/> <br />
-            <input type="text" onChange={(e)=>setCategory(e.target.value)} className="form-control" placeholder="category"/> <br />
-            <input type="file" onChange={(e)=>setImage(e.target.files[0])} className="form-control" placeholder="image"/> <br />
-            <input type="text" onChange={(e)=>setDescription(e.target.value)} className="form-control" placeholder="description"/> <br />
-            <input type="text" onChange={(e)=>setPrice(e.target.value)} className="form-control" placeholder="price"/> <br />
-            <input type="text" onChange={(e)=>setQuatity(e.target.value)} className="form-control" placeholder="quantity"/> <br />
-            <input type="text" onChange={(e)=>setDelivery_date(e.target.value)} className="form-control" placeholder="delivery date"/> <br />
-            <button onClick={addProduct} className="btn btn-primary" >Add Product</button>
+                <br />
+                <input type="text" onChange={(e) => setName(e.target.value)} className="form-control" placeholder="name" /> <br />
+                <input type="text" onChange={(e) => setCategory(e.target.value)} className="form-control" placeholder="category" /> <br />
+                <input type="file" onChange={(e) => setImage(e.target.files[0])} className="form-control" placeholder="image" /> <br />
+                <input type="text" onChange={(e) => setDescription(e.target.value)} className="form-control" placeholder="description" /> <br />
+                <input type="text" onChange={(e) => setPrice(e.target.value)} className="form-control" placeholder="price" /> <br />
+                <input type="text" onChange={(e) => setQuatity(e.target.value)} className="form-control" placeholder="quantity" /> <br />
+                <input type="text" onChange={(e) => setDelivery_date(e.target.value)} className="form-control" placeholder="delivery date" /> <br />
+                <button onClick={addProduct} className="btn btn-primary">Add Product</button>
             </div>
         </div>
     )
