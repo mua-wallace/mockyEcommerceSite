10ac67e3c24be74c8307c9c1c7fe5ec2eb41a84d
mua-wallace <muawallizewi@gmail.com>
2021-12-15 01:14:45 +0100

    add product  functionality implemented

---
+++ mocky-dashboard/src/ProductList.js
@@ -0,0 +1,20 @@
+import Header from "./Header";
+// import React, { useState, useEffect } from 'react';
+// import { Table } from 'react-bootstrap';
+function ProductList() {
+    // const [data, setData] = useState([]);
+    // useEffect(() => {
+    //     let result = fetch("http://localhost:8000/api/list");
+    //     // result = result.json();
+    //     setData(result);
+    // }, [])
+    // console.warn("result", data)
+    return (
+        <div>
+            <Header />
+            <h1>Product List</h1>
+    
+        </div>
+    )
+}
+export default ProductList;
