bdcc8c84f90fee457a5c86988b4e878390fdd8a5
mua-wallace <muawallizewi@gmail.com>
2021-12-15 01:05:47 +0100

    modified route path

--- mocky-backend/routes/api.php
+++ mocky-backend/routes/api.php
@@ -21,4 +21,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 Route::post('register', [UserController::class, 'register']);
 Route::post('login', [UserController::class, 'login']);
-Route::post('addProduct', [ProductController::class, 'addProduct']);
+Route::post('addproduct', [ProductController::class, 'addProduct']);
+// Route::get('list', [ProductController::class, 'list']);
