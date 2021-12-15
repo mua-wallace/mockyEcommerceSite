aab9c0b3e0ed8d9f02850ccdba1126e3ba43b90e
mua-wallace <muawallizewi@gmail.com>
2021-12-15 01:01:17 +0100

    modified ProductController

--- mocky-backend/app/Http/Controllers/ProductController.php
+++ mocky-backend/app/Http/Controllers/ProductController.php
@@ -9,15 +9,15 @@ class ProductController extends Controller
 {
     //
     function addProduct(Request $req) {
-        $product = new Product;
-        $product->name= $req->input('name');
-        $product->category= $req->input('category');
-        $product->description= $req->input('description');
-        $product->price= $req->input('price');
-        $product->quantity= $req->input('quantity');
-        $product->delivery_date= $req->input('delivery_date');
-        $product->file_path= $req->file('file_path')->store('products');
+        $product =new Product;
+        $product->name = $req->input('name');
+        $product->category = $req->input('category');
+        $product->description = $req->input('description');
+        $product->price = $req->input('price');
+        $product->quantity = $req->input('quantity');
+        $product->file_path = $req->file('image')->store('products');
+        $product->delivery_date = $req->input('delivery_date');
         $product->save();
-        return  $product;
+        return  $product; 
     }
 }
