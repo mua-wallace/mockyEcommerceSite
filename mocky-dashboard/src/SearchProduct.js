c44fa6b7d00dfe8e20853a62f184edddca7f58fb
mua-wallace <muawallizewi@gmail.com>
2021-12-27 13:04:14 +0100

    modified search product

--- mocky-dashboard/src/SearchProduct.js
+++ mocky-dashboard/src/SearchProduct.js
@@ -1,17 +1,54 @@
 import Header from "./Header";
-import { useState } from 'react';
-import { API_URL } from "./config/api";
+import { useState } from "react";
+import { Table } from "react-bootstrap";
 
 
-function SearchProduct() {
-   
+
+  function SearchProduct() {
+    const [data, setData] =useState([]);
+  async  function search(key) {
+      console.warn(key);
+      let result = await fetch("http://localhost:8000/api/search/"+key);
+      result = await result.json();
+      console.warn(result);
+      setData(result);
+
+    }
     return (
         <div>
             <Header />
             <div className="col-sm-6 offset-sm-3">
              <h1>Search Product</h1>
              <br />
-             <input  type="text" className="form-control" placeholder="Seach product" />
+             <input  type="text" onChange ={(e) => search(e.target.value)} className="form-control" placeholder="Seach product" />
+             <Table>
+             <tr>
+               <td>Id</td>
+               <td>Name</td>
+               <td>Category</td>
+               <td>Description</td>
+               <td>Quantity</td>
+               <td>Price</td>
+               <td>Delivery date</td>
+               <td>Image</td>
+               
+             </tr>
+             {
+                 data.map((item) =>
+                 <tr>  
+               <td>{item.id}</td>
+               <td>{item.name}</td>
+               <td>{item.category}</td>
+               <td>{item.description}</td>
+               <td>{item.quantity}</td>
+               <td>{item.price}FCFA</td>
+               <td>{item.delivery_date}</td>
+               <td><img style={{width:100}} src={"http://localhost:8000/"+item.file_path} alt=""/></td>
+             </tr>
+                 )
+             }
+           </Table>
+            
             </div>
         </div>
     )
